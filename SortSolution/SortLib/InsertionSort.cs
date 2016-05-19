7853621d01b3d456ebccdd7b1cb0109a7ac84b7d
Csvejle <ceciliavejle@outlook.dk>
2016-05-19 13:37:40 +0200

    startet

---
+++ SortSolution/SortLib/InsertionSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortLib
+{
+    public class InsertionSort : ISort
+    {
+        public T[] Sort<T>(T[] array) where T : IComparable, IComparable<T>
+        {
+            List<T> result = new List<T>();
+            if (array.Count() > 0) {
+                result.Add(array[0]);
+
+                #region Iterates through each value in the array and puts the values ordered in the result list.
+                for (int i = 1; i < array.Count(); i++)
+                {
+                    T current = array[i];
+                    int lastIndex = result.Count() - 1;
+
+                    #region Find location in result (lowest value first)
+                    bool found = false;
+                    for (int j = 0;  j < result.Count() && !found; j++)
+                    {
+                        if (result[j].CompareTo(current) > 0) {
+                            result.Insert(j, current);
+                            found = !found;
+                        }
+                        else
+                        {
+                            if (lastIndex == j)
+                            {
+                                result.Add(current);
+                                found = !found;
+                            }
+                        }
+                    }
+                    #endregion
+                }
+                #endregion
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ SortSolution/SortLib/Sort.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortLib
+{
+    public class Sort
+    {
+        MergeSort _MergeSort = new MergeSort();
+
+        InsertionSort _InsertionSort = new InsertionSort();
+
+        /* O(n) = n * log_2(n) 
+        * n is cause number of values that should be compared, log_2(n) is cause all values are splited 
+        * into a single value and then compared and putted togheter again one and one, two and two, four and four ...
+        */
+        public T[] MergeSort<T>(T[] array) where T : IComparable, IComparable<T>
+        {
+            return _MergeSort.Sort<T>(array);
+        }
+
+        /* O(n) = n^2 */
+        public T[] InsertionSort<T>(T[] array) where T : IComparable, IComparable<T>
+        {
+            return _InsertionSort.Sort<T>(array);
+        }
+    }
+
+    public interface ISort
+    {
+        /**
+            summary: can sort an unsorted array.
+        */
+        T[] Sort<T>(T[] array) where T : IComparable, IComparable<T>;
+    }
+
+    public interface IMerge
+    {
+        T[] Merge<T>(T[] array_A, T[] array_B) where T : IComparable, IComparable<T>;
+    }
+
+    public abstract class AMerge : IMerge
+    {
+        /**
+            summary: Merges two sorted lists.
+            precondition: Both lists must be sorted.
+        */
+        public T[] Merge<T>(T[] array_A, T[] array_B) where T : IComparable, IComparable<T>
+        {
+            // Convert the input arrays to lists, which gives more flexibility 
+            // and the option to resize the arrays dynamically.
+            List<T> leftList = array_A.OfType<T>().ToList();
+            List<T> rightList = array_B.OfType<T>().ToList();
+            List<T> resultList = new List<T>();
+
+            // While the sublist are not empty merge them repeatedly to produce new sublists 
+            // until there is only 1 sublist remaining. This will be the sorted list.
+            while (leftList.Count > 0 || rightList.Count > 0)
+            {
+                if (leftList.Count > 0 && rightList.Count > 0)
+                {
+                    // Compare the 2 lists, append the smaller element to the result list
+                    // and remove it from the original list.
+                    if (leftList[0].CompareTo(rightList[0]) <= 0)
+                    {
+                        resultList.Add(leftList[0]);
+                        leftList.RemoveAt(0);
+                    }
+
+                    else
+                    {
+                        resultList.Add(rightList[0]);
+                        rightList.RemoveAt(0);
+                    }
+                }
+
+                else if (leftList.Count > 0)
+                {
+                    resultList.Add(leftList[0]);
+                    leftList.RemoveAt(0);
+                }
+
+                else if (rightList.Count > 0)
+                {
+                    resultList.Add(rightList[0]);
+                    rightList.RemoveAt(0);
+                }
+            }
+
+            // Convert the resulting list back to a static array
+            return resultList.ToArray();
+        }
+    }
+
+}
